bbcfc8d6e09519c92cf118266b6af05750d7edf4
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-09-19 16:42:30 +0800

    fix: eslint and prettier conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
-import stylistic from "@stylistic/eslint-plugin";
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import stylistic from '@stylistic/eslint-plugin'
+import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
-  extends: [
-    "plugin:prettier/recommended", // Automatically turns off rules that conflict with Prettier
-  ],
-  rules: {
-    semi: ["error", "never"],
-
-    "comma-dangle": ["error", "always-multiline"],
+  files: ['**/*.ts', '**/*.js', '**/*.vue'],
+  plugins: {
+    stylistic,
   },
-}).append(stylistic.configs.recommended);
+  rules: {
+    // Style rules
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+
+    // Optional: disable formatting rules that Prettier handles
+    'prettier/prettier': 'off',
+  },
+}).append(stylistic.configs.recommended)
