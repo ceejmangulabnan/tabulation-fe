a6f5948170d935ed429fc2260c8a1b960bd7c2c0
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-10-03 16:59:27 +0800

    feat: show name, dept, and headshot

--- app/components/dashboard/DataTable.vue
+++ app/components/dashboard/DataTable.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    <v-data-table :items="participants"></v-data-table>
+    <v-data-table :headers="tableHeaders" :items="participants">
+      <template #[`item.headshot.url`]="{ value }">
+        <div @click="openDialog(value)">
+          <v-img
+            :src="`${baseApiUrl}${value}`"
+            alt="Headshot"
+            :aspect-ratio="0.8"
+            :tile="true"
+            width="100px"
+          ></v-img>
+        </div>
+      </template>
+    </v-data-table>
+
+    <v-dialog v-model="dialog" max-width="600">
+      <template #default="{ isActive }">
+        <v-card>
+          <v-img :src="currentImgUrl" contain max-height="80vh"></v-img>
+
+          <v-card-actions class="justify-end">
+            <v-btn
+              color="primary"
+              variant="text"
+              text="Close Dialog"
+              @click="isActive.value = false"
+            ></v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
   type FullParticipantsResponse = StrapiListResponse<StrapiFullParticipant>
   const participants = ref<StrapiFullParticipant[]>([])
+  const config = useRuntimeConfig()
+  const baseApiUrl = config.public.strapiUrl
+  const dialog = ref(false)
+  const currentImgUrl = ref('')
+  function openDialog(url: string) {
+    currentImgUrl.value = `${baseApiUrl}${url}`
+    dialog.value = true
+  }
 
   try {
     const api = useStrapiApi()
@@ -18,4 +55,23 @@
   } catch (error) {
     console.error('Failed to fetch populated participants:', error)
   }
+
+  const tableHeaders = [
+    {
+      title: 'Name',
+      key: 'name',
+    },
+    {
+      title: 'Department',
+      key: 'department.name',
+    },
+    {
+      title: 'Headshot',
+      key: `headshot.url`,
+    },
+  ]
 </script>
+
+<styles scoped>
+
+</styles>
